dce0392fe7f4af733c0ba756fd6d9854eb7834e7
Mark Mahdi <mmahdi2@huskers.unl.edu>
2021-08-28 01:20:54 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Mark Mahdi
+ *         mmahdi2@huskers.unl.edu
+ * Date: 2020/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mark Mahdi \n");
+
+  return 0;
+}
